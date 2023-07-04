818b9a0511abeed532495a8fa377f2b44d136319
steven vasquez <ministeven10@gmail.com>
2023-07-03 20:51:46 -0700

    Created responsive nav bar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,12 +1,53 @@
-export function Navbar() {
-    return (
-        <div id="nav-bar">
-            <h1>Steven Vasquez</h1>
-            <ul id="nav-bar-list">
-                <li>About</li>
-                <li>Projects</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-    );
+import { NavLink } from 'react-router-dom';
+import '../stylesheets/Navbar.css';
+import { Component } from 'react';
+
+export class Navbar extends Component {
+    state = { clicked: false };
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked });
+    };
+
+    render() {
+        return (
+            <>
+                <nav>
+                    <div id="home-link">
+                        <NavLink exact to="/" activeClassName="active">
+                            Steven Vasquez
+                        </NavLink>
+                    </div>
+            
+                    <ul id="nav-bar-links" className={this.state.clicked ? "#navbar active" : "#navbar"}>
+                        <li>
+                            <NavLink exact to="/" activeClassName="active">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about" activeClassName="active">
+                                About
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/projects" activeClassName="active">
+                                Projects
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact" activeClassName="active">
+                                Contact
+                            </NavLink>
+                        </li>
+                    </ul>
+
+                    <div id="mobile" onClick={this.handleClick}>
+                        <i id="bar" className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                    </div>
+                </nav>
+            </>
+        );
+    }
 }
+
+export default Navbar;
