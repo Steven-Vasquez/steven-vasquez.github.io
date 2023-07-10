9c265de0de52e96bb363f606ecd38c23bd17e1f1
steven vasquez <ministeven10@gmail.com>
2023-07-10 15:55:08 -0700

    Created and styled layout for Projects section

--- my-app/src/components/BioSection.js
+++ my-app/src/components/BioSection.js
@@ -6,10 +6,10 @@ export function Biography() {
         <>
             <div className="bio-section">
                 <div className="bio-header">
-                    <h1 style={{ display: 'flex', flexDirection: 'row' }}>
+                    <h1 style={{ display: 'flex', flexDirection: 'row', fontWeight: 400 }}>
                         <div>My</div>
                         <span>&nbsp;</span>
-                        <div className="highlight"> 
+                        <div className="highlight">
                             <div>Skills</div>
                             <div></div>
                         </div>
@@ -23,33 +23,32 @@ export function Biography() {
                 </div>
 
                 <div className="bio">
-                    <div id="skills-container">
-                        <ul className="skills">
-                            <li className="skill-box">Java</li>
-                            <li className="skill-box">Python</li>
-                            <li className="skill-box">JavaScript</li>
-                            <li className="skill-box">TypeScript</li>
-                            <li className="skill-box">C++</li>
-                            <li className="skill-box">MATLAB</li>
-                            <li className="skill-box">R</li>
-                            <li className="skill-box">SQL</li>
-                            <li className="skill-box">PostgreSQL</li>
-                            <li className="skill-box">HTML</li>
-                            <li className="skill-box">CSS</li>
-                            <li className="skill-box">React</li>
-                            <li className="skill-box">Node.js</li>
-                            <li className="skill-box">Express.js</li>
-                            <li className="skill-box">Git</li>
-                            <li className="skill-box">AWS</li>
-                        </ul>
-                    </div>
-                    
-                    
+                    <ul className="skills">
+                        <li className="skill-box">Java</li>
+                        <li className="skill-box">Python</li>
+                        <li className="skill-box">JavaScript</li>
+                        <li className="skill-box">TypeScript</li>
+                        <li className="skill-box">C++</li>
+                        <li className="skill-box">MATLAB</li>
+                        <li className="skill-box">R</li>
+                        <li className="skill-box">SQL</li>
+                        <li className="skill-box">PostgreSQL</li>
+                        <li className="skill-box">HTML</li>
+                        <li className="skill-box">CSS</li>
+                        <li className="skill-box">React</li>
+                        <li className="skill-box">Node.js</li>
+                        <li className="skill-box">Express.js</li>
+                        <li className="skill-box">Git</li>
+                        <li className="skill-box">AWS</li>
+                    </ul>
+
+
+
                     <div id="bio-text-container">
                         <b>(Hello)</b>
                         <p id="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
                         <NavLink exact to="/" activeClassName="active" id="about-page-link">
-                            <b style={{fontSize: ".85em", fontWeight: "1000"}}>Read my full bio</b>
+                            <b style={{ fontSize: ".85em", fontWeight: "1000" }}>Read my full bio</b>
                         </NavLink>
                     </div>
 
--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,22 +1,67 @@
 import "../stylesheets/Projects.css"
 
+
+// IndividualProject component
+function IndividualProject(props) {
+    const { projectRole, projectTitle, techSkills, projectLink, projectDescription, projectImages } = props;
+
+    return (
+        <div className="individual-project">
+            <div className="project-role">{projectRole}</div>
+            <h1 className="project-name">{projectTitle}</h1>
+            <ul className="skills-used">
+                {techSkills.map((skill) => (
+                    <li key={skill} className="skill-box">{skill}</li>
+                ))}
+            </ul>
+            <div className="project-button">
+                <a tabIndex="0" href={projectLink}>View Project</a>
+                <a tabIndex="1" href={projectLink}>View Project</a>
+            </div>
+            <p className="project-description">{projectDescription}</p>
+            <div className="project-images">
+                {projectImages.map((image) => (
+                    <img src={image} alt="Project Image" key={image} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
 export function Projects() {
+    const projectData = {
+        projectRole: "LEAD BACKEND DEVELOPER",
+        projectTitle: "ULingual",
+        techSkills: ["JavaScript", "React", "NodeJS", "ExpressJS", "SQL", "AWS", "Twilio API"],
+        projectDescription: "Served as backend lead in a team of 6 to create a language learning web app that connects native speakers to exchange language and culture. The site features ",
+        projectLink: "https://example.com",
+        projectImages: [
+            "",
+            "",
+            ""
+        ]
+    };
+
     return (
         <>
             <div className="projects-section">
                 <div className="projects-header">
-                    <h1 style={{ display: 'flex', flexDirection: 'row' }}>
-                        <div>My</div>
-                        <span>&nbsp;</span>
+                    <h1 style={{ display: 'flex', flexDirection: 'row'}}>
                         <div className="highlight">
                             <div>Projects</div>
                             <div></div>
                         </div>
                     </h1>
                 </div>
+
+                <div className="projects">
+                    <IndividualProject {...projectData} />
+
+                </div>
             </div>
         </>
     );
 }
+
 
 export default Projects;
